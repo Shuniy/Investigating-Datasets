3cec0c82699fc3c31ddd4de7d9e09c77b7a7f8c2
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-06-09 14:19:00 +0530

    Updated

--- Enron Dataset/tools/parse_out_email_text.py
+++ Enron Dataset/tools/parse_out_email_text.py
@@ -3,6 +3,8 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 
+sys.path.append("D:/ud120-projects/")
+
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
@@ -50,7 +52,7 @@ def parseOutText(f):
     
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
+    ff = open("text_learning/test_email.txt", "r")
     text = parseOutText(ff)
     print (text)
 
